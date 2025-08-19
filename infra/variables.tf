5c20f467a2390da17751118dc14ee5e91ab68236
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-08-18 22:45:13 -0300

    feat: Add variables for AWS deployment configuration

---
+++ infra/variables.tf
@@ -0,0 +1,29 @@
+variable "account_username" {
+  description = "The AWS account username"
+  type        = string
+  nullable = false
+}
+
+variable "region" {
+  description = "The AWS region to deploy the resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "The environment for the deployment (e.g., dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "app_name" {
+  description = "The app_name of the deployment (e.g., static-site, api)"
+  type        = string
+  nullable = false
+}
+
+variable "project" {
+  description = "The project name for tagging purposes"
+  type        = string
+  nullable = false
+}
