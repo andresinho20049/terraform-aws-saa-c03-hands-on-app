7190d4f8c483d91d27d0490654687032a239d7a5
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-08-20 00:55:25 -0300

    feat: add new badge images and update BadgeComponent for better display

--- src/components/footer/badge.tsx
+++ src/components/footer/badge.tsx
@@ -1,16 +1,18 @@
-import { BadgesType } from "@/my-app/hooks/use-bedges"
-import Link from "next/link"
+import { BadgesType } from '@/my-app/hooks/use-badges';
+import Link from 'next/link';
 
-export const BadgeComponent = ({
-    name,
-    url,
-    src,
-    alt
-}:BadgesType) => {
-    return (
-        <Link href={url} target="_blank" rel="noopener noreferrer" className="group relative">
-            <img src={src} alt={alt} />
-            <span className="group-hover:block hidden absolute text-center">{name}</span>
-        </Link>
-    )
-}
+export const BadgeComponent = ({ name, url, src, alt }: BadgesType) => {
+  return (
+    <Link
+      href={url}
+      target='_blank'
+      rel='noopener noreferrer'
+      className='group relative'
+    >
+      <img src={src} alt={alt} className='w-32' />
+      <div className='absolute bottom-0 left-0 hidden w-full bg-black bg-opacity-75 p-2 text-center text-sm underline group-hover:block'>
+        <span>{name}</span>
+      </div>
+    </Link>
+  );
+};
