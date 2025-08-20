8968b84d37843cfe15e5178597361e8c04daffe4
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-08-20 18:11:28 -0300

    fix: Adjust reading of the badge component in the light theme

--- src/components/footer/badge.tsx
+++ src/components/footer/badge.tsx
@@ -10,8 +10,8 @@ export const BadgeComponent = ({ name, url, src, alt }: BadgesType) => {
       className='group relative'
     >
       <img src={src} alt={alt} className='w-32' />
-      <div className='absolute bottom-0 left-0 hidden w-full bg-black bg-opacity-75 p-2 text-center text-sm underline group-hover:block'>
-        <span>{name}</span>
+      <div className='absolute bottom-0 left-0 hidden w-full bg-black bg-opacity-60 p-2 text-center text-sm underline group-hover:block'>
+        <span className='group-hover:text-white'>{name}</span>
       </div>
     </Link>
   );
