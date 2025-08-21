36faf139cc8f6368bacbc4f898cb7b82957cf788
Andresinho20049 <andre.andresinho2009@hotmail.com>
2025-08-21 14:40:57 -0300

    feat: Add new `ResourcePage` page
    This commit adds the new page to the project. The page is designed to showcase learning materials and projects related to the AWS Solutions Architect certification.
    
    The new page was developed using the project's updated modular components and styling standards, ensuring consistency and a clean design.

---
+++ src/app/[lang]/resource/page.tsx
@@ -0,0 +1,55 @@
+import { generateStaticParams } from '@/my-app/hooks/use-languages';
+import { PageProps } from '../layout';
+import { getDictionary } from '../../dictionaries';
+import { CardImageWithTitle } from '@/my-app/components/card/card-image-with-title';
+import {
+  WaveDiviver,
+  WaveDiviverInvert,
+} from '@/my-app/components/dividers/wave-divider';
+import { SectionIntroduceToLink } from '@/my-app/components/sections/section-introduce-to-link';
+import { TitlePage } from '@/my-app/components/title/title-page';
+import { TitleToSection } from '@/my-app/components/title/title-section';
+
+export { generateStaticParams };
+
+export default async function ResourcePage({ params }: PageProps) {
+  const { lang } = await params;
+  const dictionary = await getDictionary(lang);
+  return (
+    <main className='min-h-screen w-full'>
+      <TitlePage
+        title={dictionary.resource.title}
+        description={dictionary.resource.description}
+        customClassName='pt-20'
+      >
+        <img
+          src={dictionary.resource.imgSrc}
+          alt={dictionary.resource.title}
+          className='w-full max-w-3xl'
+        />
+      </TitlePage>
+      <WaveDiviverInvert />
+      <TitleToSection
+        title={dictionary.resource.subtitle}
+        description={dictionary.resource.descriptionResources}
+        customClassName='bg-second-light dark:bg-second-dark'
+      />
+
+      <div className='w-full divide-y-2 bg-second-light px-4 dark:bg-second-dark lg:px-20'>
+        {dictionary.resource.resourcesList.map((resource) => (
+          <SectionIntroduceToLink
+            title={resource.title}
+            description={resource.description}
+            linkButton={resource.link}
+            cardImage={{
+              cardTitle: resource.cardTitle,
+              imageSrc: resource.imgSrc,
+            }}
+            key={resource.title}
+          />
+        ))}
+      </div>
+      <WaveDiviver />
+    </main>
+  );
+}
